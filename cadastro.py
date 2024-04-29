07cae78f38da16bdca6727cd1caa335d94fcaf16
vania20231 <129951429+vania20231@users.noreply.github.com>
2024-04-29 12:14:52 -0300

    Create cadastro.py

---
+++ cadastro.py
@@ -0,0 +1,23 @@
+nome = input ("Digite seu nome" )
+idade = input ("Digite seu idade")
+sexo = input ("Diite seu sexo") 
+
+nome2 = input ("Digite seu nome" )
+idade2 = input ("Digite seu idade")
+sexo2 = input ("Diite seu sexo") 
+
+nome3 = input ("Digite seu nome" )
+idade3 = input ("Digite seu idade")
+sexo3 = input ("Diite seu sexo") 
+
+print ("nome:", nome )
+print ("idade:", idade)
+print ("sexo:", sexo)
+
+print ("nome:", nome2)
+print ("idade:", idade2)
+print ("sexo:", sexo2)
+
+print ("nome:", nome3)
+print ("idade:", idade3)
+print ("sexo:", sexo3)
