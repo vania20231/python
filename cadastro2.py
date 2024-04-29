a5aff1076d626d4ad3462e8033954919f7ada94f
vania20231 <129951429+vania20231@users.noreply.github.com>
2024-04-29 12:32:54 -0300

    Create cadastro2.py

---
+++ cadastro2.py
@@ -0,0 +1,39 @@
+nome = input ("Digite seu nome" )
+idade = int (input ("Digite seu idade"))
+sexo = input ("Diite seu sexo") 
+
+nome2 = input ("Digite seu nome" )
+idade2 = int (input ("Digite seu idade"))
+sexo2 = input ("Diite seu sexo") 
+
+nome3 = input ("Digite seu nome" )
+idade3 = int(input ("Digite seu idade"))
+sexo3 = input ("Diite seu sexo") 
+
+
+print ("nome:", nome )
+print ("idade:", idade)
+if(idade >= 18 ):
+    print ("maior de idade")    
+else:
+    print("menor de idade")
+
+print ("sexo:", sexo)
+
+print ("nome:", nome2)
+print ("idade:", idade2)
+if(idade2 >= 18 ):
+    print ("maior de idade")    
+else:
+    print("menor de idade")
+
+print ("sexo:", sexo2)
+
+print ("nome:", nome3)
+print ("idade:", idade3)
+if(idade3 >= 18 ):
+    print ("maior de idade")    
+else:
+    print("menor de idade")
+
+print ("sexo:", sexo3)
