2054b40229dc0ba39d096e18ec8ca1cb07d60655
vania20231 <129951429+vania20231@users.noreply.github.com>
2024-06-04 10:55:48 -0300

    Update forca.py

--- forca.py
+++ forca.py
@@ -8,21 +8,21 @@ def forca(tentativa):
     f7 = "__|___________"
 
     if tentativa>= 1:
-        f2="|         |"
+        f2="  |         | "
     if tentativa>= 2:
-        f3="|         o "
+        f3="  |         o "
     if tentativa>= 3:
-        f4="|         | "
+        f4="  |         | "
     if tentativa>= 4:
-        f4="|        /| " 
+        f4="  |        /| " 
     if tentativa>=5:
-        f4="|        /|\ "     
+        f4="  |        /|\ "     
     if tentativa>=6:
-        f5="|         |"  
+        f5="  |         |"  
     if tentativa>=7:
-        f6="|        /   "
+        f6="  |        /   "
     if tentativa>=8:  
-        f6="|          \ "  
+        f6="  |        / \ "  
 
     print(f1)
     print(f2)
@@ -31,17 +31,6 @@ def forca(tentativa):
     print(f5)
     print(f6)
     print(f7)
-
-
-forca(0)
-
-
-
-
-
-
-
-
 
 
 def continua():
@@ -58,9 +47,27 @@ def continua():
             print("Digite S ou N ")
     return acabou
      
-jogar = True
-x=0
-while jogar :
-    print(x)
-    forca(x)
-    jogar = continua()
+##jogar = True
+##x=0
+##while jogar :
+    ##print(x)
+    ##forca(x)
+    ##jogar = continua()
+forca(0)
+
+
+def sorteiapalavra ():
+    lista = ["amor","paz","prosperidade","ajuda","salvação"]
+    return random.choice (lista)
+
+import random
+
+
+
+
+
+
+print(sorteiapalavra())
+
+
+
