b658db2d0ef289137b0e2d132803d675db23ea66
vania20231 <129951429+vania20231@users.noreply.github.com>
2024-06-17 08:52:54 -0300

    Create fruta.py

---
+++ fruta.py
@@ -0,0 +1,11 @@
+listafrutas=["morango","manga","mamão"]
+fruta= "morango"
+print(listafrutas[1])
+print(fruta[1])
+print(listafrutas[2][0])
+listafrutas.append("abacaxi")
+print(listafrutas)
+listafrutas.remove("mamão")
+print(listafrutas)
+listafrutas.sort()
+print(listafrutas)
