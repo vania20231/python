69bd6a7249b575ba23e7322e79963fcdee34ae56
vania20231 <129951429+vania20231@users.noreply.github.com>
2024-06-24 08:49:53 -0300

    Update forca.py

--- forca.py
+++ forca.py
@@ -1,28 +1,28 @@
-def forca(tentativa):
-    f1 = "  +--------+  "
-    f2 = "  |           "
-    f3 = "  |           "
-    f4 = "  |           "
-    f5 = "  |           "
-    f6 = "  |           "
-    f7 = "__|___________"
+def Forca(Tentativa):
+    f1 = "    +-------+  " 
+    f2 = "    |          "
+    f3 = "    |          "
+    f4 = "    |          "
+    f5 = "    |          "
+    f6 = "    |          "
+    f7 = "____|______    "
 
-    if tentativa>= 1:
-        f2="  |         | "
-    if tentativa>= 2:
-        f3="  |         o "
-    if tentativa>= 3:
-        f4="  |         | "
-    if tentativa>= 4:
-        f4="  |        /| " 
-    if tentativa>=5:
-        f4="  |        /|\ "     
-    if tentativa>=6:
-        f5="  |         |"  
-    if tentativa>=7:
-        f6="  |        /   "
-    if tentativa>=8:  
-        f6="  |        / \ "  
+    if Tentativa >= 1:
+        f2 = "    |       |  "
+    if Tentativa >= 2:
+        f3 = "    |       O  "
+    if Tentativa >= 3:
+        f4 = "    |       |   "
+    if Tentativa >= 4:
+        f4 = "    |      /|  "
+    if Tentativa >= 5:
+        f4 = "    |      /|\ "
+    if Tentativa >= 6:
+        f5 = "    |       |  "
+    if Tentativa >= 7:
+        f6 = "    |      /   "
+    if Tentativa >= 8:
+        f6 = "    |      / \ "
 
     print(f1)
     print(f2)
@@ -32,47 +32,94 @@ def forca(tentativa):
     print(f6)
     print(f7)
 
-
-def continua():
+def Continua():
     while True:
-        print("-" * 20 )
-        novamente = input("Quer jogar de novo S/N: "). upper()
+        print("-" * 20)
+        novamente = input("Quer jogar de novo S/N: ").upper()
         if novamente == "S":
-            acabou = True
+            NAcabou = True 
             break
         elif novamente == "N":
-            acabou = False
+            NAcabou = False
             break
         else:
             print("Digite S ou N ")
-    return acabou
+    return NAcabou
 
-def apresentapalavra(letras,palavras):
-    npalavra="_ "*len(palavras)
-    for l in range(0,len(letras)):
-        print(letras[l])
-        for p in range(0,len(palavras)):
-            ##print(palavras[p])
-            if letras[l]==palavras[p]:
-                print(letras[l])
-                print(l)
-                print(p)
-    return npalavra
+def SorteiaPalavra():
+    lista = ["AMOR", "MELANCIA", "CATEDRAL", "ESCOLA", "MATEMÁTICA",
+             "REFEITÓRIO", "DIRETORIA", "CAMINHÃO", "CACHORRO"]
+    return random.choice(lista)
+    
+def ApresentaPalavra(letras, palavra):
+    PalavraCifrada = "_ " * len(palavra)
+    for l in range(0, len(letras)) :
+        for p in range(0, len(palavra)) :
+            if palavra[p] == letras[l] :
+                PalavraCifrada = PalavraCifrada[0:(p*2)] + palavra[p] + " " + PalavraCifrada[(p*2)+2:] 
+    print(PalavraCifrada)
 
-def sorteiapalavra ():
-    lista = ["amor","paz","prosperidade","ajuda","salvação"]
-    return random.choice (lista)
-     
-##jogar = True
-##x=0
-##while jogar :
-    ##print(x)
-    ##forca(x)
-    ##jogar = continua()
-forca(0)
+def DigiteUmaLetra(listaletra):
+    novaLetra = input("Digite uma letra da Palavra: ").upper()
+    while True:
+        if listaletra.find(novaLetra) >=0 :
+            novaLetra = input("Letra já digitada. Digite outra letra da Palavra: ").upper()
+        else:
+            break
 
+    listaletra = listaletra + novaLetra
+    return listaletra
 
+def AcertouQuantas(letras, palavra) :
+    corretas = 0
+    for l in range(0, len(letras)) :
+        for p in range(0, len(palavra)) :
+            if palavra[p] == letras[l] :
+                corretas = corretas + 1
+    return corretas
+
+def Placar(Ganhou, Perdeu):
+    T = 27
+    print("="*T)
+    print(f'= Ganhou: {Ganhou:-2} | Perdeu: {Perdeu:-2} =')
+    print("="*T)
+
+import os
 import random
-print(sorteiapalavra())
-
-print(apresentapalavra("abx","amor"))
+Jogar = True
+Ganhou = 0 
+Perdeu = 0
+while Jogar :
+    PalavraSorteada = SorteiaPalavra()
+    ListaLetra = " "
+    Tentativas = 7
+    NaoAcertou = True
+    SituacaoForca = -1
+    AcertosAnterior = 0
+    while SituacaoForca != Tentativas:
+        os.system('cls' if os.name == 'nt' else 'clear')
+        Acertos = AcertouQuantas(ListaLetra, PalavraSorteada)
+        if Acertos == len(PalavraSorteada):
+            Ganhou = Ganhou + 1
+            Placar(Ganhou, Perdeu)
+            print("Parabéns, você acertou.") 
+            ApresentaPalavra(ListaLetra, PalavraSorteada)
+            NaoAcertou = False
+            break
+        if Acertos == AcertosAnterior:
+            SituacaoForca = SituacaoForca + 1
+        AcertosAnterior = Acertos
+        Placar(Ganhou, Perdeu)
+        Forca(SituacaoForca)
+        ApresentaPalavra(ListaLetra, PalavraSorteada)
+        print("Letras já digitadas: "+ListaLetra)
+        ListaLetra = DigiteUmaLetra(ListaLetra)
+        
+    if NaoAcertou :
+        os.system('cls' if os.name == 'nt' else 'clear')
+        Perdeu = Perdeu + 1
+        Placar(Ganhou, Perdeu)
+        Forca(SituacaoForca+1)
+        ApresentaPalavra(ListaLetra, PalavraSorteada)
+        print("Você errou, Tente novamente.")
+    Jogar = Continua()
