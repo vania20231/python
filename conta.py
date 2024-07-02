89b06400811c97f10519d91e918e68a6851e59a8
vania20231 <129951429+vania20231@users.noreply.github.com>
2024-07-02 10:37:21 -0300

    Update conta.py

--- conta.py
+++ conta.py
@@ -6,12 +6,31 @@ def saldo(conta):
     print("conta numero:{}".format(conta["numero"]))
     print("titular:"+conta["titular"])
     print("saldo:{}".format(conta["saldo"]))
+
 def deposito(conta,valor):
 
     conta["saldo"] +=valor
+
+def extrato (conta):
+    print("conta {} saldo {}". format(conta["numero"], conta ["saldo"]))
+
+def deposito (conta, valor):
+    conta["saldo"] += valor
+
+def sacar(conta, valor):
+    conta["saldo"] -= valor    
     
 conta00= cria_conta(1234, "Arisberto", 50.0, 100.0)
 conta01= cria_conta(4321,"Leandro", 50.0,100.0)
 
 saldo(conta00)
 saldo(conta01)
+
+deposito(conta00, 100.0)
+extrato(conta00)
+deposito(conta01, 80.0)
+extrato(conta01)
+sacar(conta00, 30.0)
+sacar(conta01, 20.0)
+extrato(conta00)
+extrato(conta01)
