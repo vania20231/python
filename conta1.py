75fde7378b351bfb0d6256e3e6c6c5fe0e5b2d29
vania20231 <129951429+vania20231@users.noreply.github.com>
2024-07-29 09:00:11 -0300

    Create conta1.py

---
+++ conta1.py
@@ -0,0 +1,14 @@
+class conta:
+    def __ini__(self, numero, titular, saldoinicial,limite):
+        self.numero=numero
+        self.titular=titular
+        self.saldo=saldoinicial
+        self.limite=limite         
+
+
+
+
+
+
+conta1 = conta(134, "Leandro", 600.00, 50.000)
+print (conta1)
