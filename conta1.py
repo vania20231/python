66a08c8f5a14eeec0f9a6436bd37bc8cf0db6edb
vania20231 <129951429+vania20231@users.noreply.github.com>
2024-08-05 12:00:01 -0300

    Update conta1.py

--- conta1.py
+++ conta1.py
@@ -3,7 +3,25 @@ class conta:
         self.numero=numero
         self.titular=titular
         self.saldo=saldoinicial
-        self.limite=limite         
+        self.limite=limite
+
+def extrato(salf):
+    print("Saldo atual: {}".format(self.__saldo))
+
+def __podeSacar(self,valorSacar):
+    valorDisponivel = self.__saldo + self.limite
+    return valorSacar 
+
+def transfere(self, valor,destino):
+    self.__saldo -= valor
+    destino.deposita(valor)
+
+def get_numero(self):
+    return self._numero
+
+@property
+def numero  (self):
+    return self.__numero             
 
 
 
